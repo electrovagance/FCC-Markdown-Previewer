7953b3dc86363e7a6f1d6d1863d58e8dd42736c9
Malak Tayeh <malaktayeh22@gmail.com>
2019-02-17 18:14:44 -0500

    built basic app layout

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1> Build a Markdown Previewer</h1>
+        <section id="main-app-area">
+          <textarea rows="50" id="editor">
+            <p># This is a header</p>
+          </textarea>
+          <div id="preview">
+          </div>
+        </section>
       </div>
-    );
+    )
   }
 }
 
